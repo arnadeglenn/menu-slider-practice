a71d01f1e062187764f4f036684b06b4ec520276
arnadeglenn <125632656+arnadeglenn@users.noreply.github.com>
2023-10-29 22:46:45 -0700

    Create nav menu with hamburger icon

--- index.js
+++ index.js
@@ -4,17 +4,38 @@ const impactNav = document.querySelector('.impact-nav');
 const aboutDrop = document.querySelector('.about-drop');
 const productDrop = document.querySelector('.product-drop');
 const impactDrop = document.querySelector('.impact-drop');
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
 
 
 
 const navDropDown = function(navItem, dropItem) {
     navItem.addEventListener('mouseenter', (e) => {
-        dropItem.style.display = 'block';
+        dropItem.style.display = 'flex';
     })
     navItem.addEventListener('mouseleave', (e) => {
         dropItem.style.display = 'none';
     })
 }
+
+const hamburgerNav = function () {
+    hamburger.addEventListener('click', (e) => {
+        console.log('clicked')
+        if (nav.style.display === 'none' || nav.style.display === '') {
+            nav.style.display = 'flex';
+            document.body.style.overflowY = 'hidden';
+        } else {
+            nav.style.display = 'none';
+            document.body.style.overflowY = 'visible';
+        }
+    })
+}
+
+if (window.innerWidth < 321) {
+    hamburgerNav();
+}
+
+
 
 navDropDown(aboutNav, aboutDrop);
 navDropDown(productNav, productDrop);
